e043b127e2559fa55b3e4428d193f7091bded29c
bdgre <bdgre@DESKTOP>
2022-11-03 13:27:43 -0400

    Add project files.

---
+++ Models/Posts.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HilltopBlazorWASM.Models
+{
+    public class Posts
+    {
+        public Data[] data { get; set; }
+
+        public class Data
+        {
+            public int id { get; set; }
+            public Attributes? attributes { get; set; }
+
+        }
+        public class Attributes
+        {
+            public string? Title { get; set; }
+            public string? Content { get; set; }
+            public string? Author { get; set; }
+            public string? Slug { get; set; }
+
+        }
+
+        public string TrimString(string input)
+        {
+            String output;
+            output = input;
+            return output;
+        }
+    }    
+}
---
+++ Models/Quiz/Quizzes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HilltopBlazorWASM.Models.Quiz {
+    public class Quizzes {
+
+        public Data[] data { get; set; }
+
+        public class Data {
+            public int id { get; set; }
+            public Attributes? Attributes { get; set; }
+
+        }
+        public class Attributes {
+            public string? Title { get; set; }
+            public Question[] Question { get; set; }
+        }
+        public class Question {
+            public int id { get; set; }
+            public string? QuestionText { get; set; }
+            public PossibleAnswer[] PossibleAnswer { get; set; }
+
+        }
+        public class PossibleAnswer {
+            public int Id { get; set; }
+            public string? AnswerText { get; set; }
+            public bool Correct { get; set; }
+        }
+    }
+    
+
+   
+}
