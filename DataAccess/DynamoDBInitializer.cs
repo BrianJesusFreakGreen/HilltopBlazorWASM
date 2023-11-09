349d3bc5d6d75ae5c6f1b19708568ca3e07cccc0
BrianJesusFreakGreen <114704460+BrianJesusFreakGreen@users.noreply.github.com>
2023-11-09 15:33:06 -0500

    f

--- DataAccess/DynamoDBInitializer.cs
+++ DataAccess/DynamoDBInitializer.cs
@@ -5,6 +5,7 @@ using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.DynamoDBv2.Model;
 using Amazon.Runtime;
 using Amazon.Runtime.Endpoints;
+using Amazon.Util;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,7 +15,11 @@ namespace HilltopBlazorWASM.DataAccess {
         
         public static AmazonDynamoDBClient client;
         public static async Task InitializeDynamoDB(string tableName = "") {
-            client = new AmazonDynamoDBClient( );
+            Console.WriteLine("Trying to do crap");
+            client = new AmazonDynamoDBClient(RegionEndpoint.USEast1);
+            var response = client.ListTablesAsync();
+            Console.WriteLine("Testing");
+            Console.WriteLine(response);
             DynamoDBContext context = new DynamoDBContext(client);
             Task<HilltopTest> testItem = context.LoadAsync<HilltopTest>("Brian");
             Console.WriteLine(testItem.Result);
@@ -30,6 +35,6 @@ namespace HilltopBlazorWASM.DataAccess {
     public class HilltopTest {
         [DynamoDBHashKey]
         public string Hilltop {  get; set; }
-        [DynamoDBRangeKey] public string data { get; set; }
+         public string Data { get; set; }
     }
 }
