f83dc2f85b358092cd952df687d4501dfa8fede1
BrianJesusFreakGreen <114704460+BrianJesusFreakGreen@users.noreply.github.com>
2023-11-12 07:55:59 -0500

    a

---
+++ Models/AChurchMinistry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace HilltopBlazorWASM.Models {
+    public class AChurchMinistry {
+        
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string Status { get; set; }
+        
+        public AChurchMinistry() {
+            
+        }
+    
+    }
+   
+}
---
+++ Models/AEvent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+namespace HilltopBlazorWASM.Models {
+    public class AEvent{
+        public Data data {  get; set; }
+        public class Data {
+
+
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string Location { get; set; }
+            public string ContactName { get; set; }
+            public int ContactNumber { get; set; }
+            public string ContactEmail { get; set; }
+
+            public DateTime Time { get; set; }
+            public bool MoreDetails { get; set; }
+        }
+        
+    
+    }
+
+
+}
