0e69da908f3d248c7cf4a3e4951515ac53182320
BrianJesusFreakGreen <114704460+BrianJesusFreakGreen@users.noreply.github.com>
2023-11-12 08:20:55 -0500

    s

--- Models/PrayerRequest.cs
+++ Models/PrayerRequest.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using Amazon.DynamoDBv2.DataModel;
 
 namespace HilltopBlazorWASM.Models
 {
-    [DynamoDBTable("PrayerRequest")]
     public class PrayerRequest{
-        [DynamoDBHashKey]
         public string PrayerID { get; set; }
         public string PrayerOwner { get; set; }
         public string PrayerContent { get; set; }
